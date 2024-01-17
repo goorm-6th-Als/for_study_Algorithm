20b82eedda8caade71bca29f310b0dafe66d30c5
Zero <109202222+savedinstancestate@users.noreply.github.com>
2024-01-17 14:29:25 +0900

    [fix] BOJ 1475 방 번호

--- SeoDaYoung/백준/BOJ_1475_방_번호.java
+++ SeoDaYoung/백준/BOJ_1475_방_번호.java
@@ -6,7 +6,7 @@ public class BOJ_1475_방_번호 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s = br.readLine();
-        int[] arr = new int[9];
+        int[] arr = new int[10];
 
         for (char c : s.toCharArray()) { // 문자열을 한 글자식 쪼개 char타입 배열로 변환
             int num = c - '0';
@@ -20,7 +20,7 @@ public class BOJ_1475_방_번호 {
         arr[6] = arr[6]/2 + arr[6]%2;
 
         Arrays.sort(arr); // 배열 오름차순 정렬
-        System.out.println(arr[8]); // 각 자리의 수를 카운트하므로 맨 끝 배열 출력
+        System.out.println(arr[arr.length-1]); // 각 자리의 수를 카운트하므로 맨 끝 배열 출력
 
     }
 }
