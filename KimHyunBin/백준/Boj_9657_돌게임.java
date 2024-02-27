0e746a2128521f57668c92d32327d2d7598a1619
김현빈 <96505736+khv9786@users.noreply.github.com>
2024-02-27 14:48:28 +0900

    [solved] Boj_9657_돌게임

--- KimHyunBin/백준/Boj_9657_돌게임.java
+++ KimHyunBin/백준/Boj_9657_돌게임.java
@@ -1,22 +1,19 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {// Boj_9657_돌게임
-    // DP
-    public static int N;
-    public static boolean[] dp;
+public class Main {// Boj_2502_떡 먹는 호랑이
+    // DP 탑 다운?
+    public static int D, K, A, B;
+    public static int [] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        dp = new boolean[Math.max(N+1,4)];
-        // 상근 먼저 시작, SK, CY
-        dp[1] = true;
-        dp[3] = true;
-        dp[4] = true;
+        D = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int [31];
 
         // 상근이가 이긴 숫자에서 +1,+3,+4 라면 창연 승
         for(int i = 5; i<=N; i++){
